afed24b8b0df5d0be3a69b7e9d5f0d46c1450e9e
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-03-25 19:37:50 +0800

    Add possible enhancement as comments to entity

--- internal/shows/entities/comment.go
+++ internal/shows/entities/comment.go
@@ -1,5 +1,7 @@
 package entities
 
+// ENHANCEMENT: set default field value
+// ENHANCEMENT: leverage UUID to prevent IDOR (insecure direct object references)
 type Comment struct {
 	Id      int
 	UserId  int
